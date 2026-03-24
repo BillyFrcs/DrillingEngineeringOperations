35a01358e94bcf967547169a22bdea80f6e2b290
Billy Franscois <BillyFranscois@outlook.com>
2026-03-24 22:26:34 +0700

    Update API Hydraulics Equations

--- APIHydraulicsEquations/PressureLosses/main.py
+++ APIHydraulicsEquations/PressureLosses/main.py
@@ -1,8 +1,8 @@
 import math
 
-'''
+"""
 Pressure Losses Calculations in PSI and BAR
-'''
+"""
 surface_line_pressure_drop_psi = 41.53
 surface_line_pressure_drop_bar = 2.86
 
@@ -21,10 +21,25 @@ annular_interval2_drill_pipe_bar = 1.05
 annular_interval3_casing_drill_pipe_psi = 178.44
 annular_interval3_casing_drill_pipe_bar = 12.30
 
-psi = surface_line_pressure_drop_psi + drill_string_interval1_drill_pipe_psi + drill_string_interval2_drill_collar_psi + annular_interval1_drill_collar_psi + annular_interval2_drill_pipe_psi + annular_interval3_casing_drill_pipe_psi
-bar = surface_line_pressure_drop_bar + drill_string_interval1_drill_pipe_bar + drill_string_interval2_drill_collar_bar + annular_interval1_drill_collar_bar + annular_interval2_drill_pipe_bar + annular_interval3_casing_drill_pipe_bar
+psi = (
+    surface_line_pressure_drop_psi
+    + drill_string_interval1_drill_pipe_psi
+    + drill_string_interval2_drill_collar_psi
+    + annular_interval1_drill_collar_psi
+    + annular_interval2_drill_pipe_psi
+    + annular_interval3_casing_drill_pipe_psi
+)
+
+bar = (
+    surface_line_pressure_drop_bar
+    + drill_string_interval1_drill_pipe_bar
+    + drill_string_interval2_drill_collar_bar
+    + annular_interval1_drill_collar_bar
+    + annular_interval2_drill_pipe_bar
+    + annular_interval3_casing_drill_pipe_bar
+)
 
 bit_pressure_drop = 3000.00 - psi
 
-print(f'Total Pressure Lost = {psi} psi {bar} bar')
-print(f'Bit Pressure Drop = {bit_pressure_drop} psi')
+print(f"Total Pressure Lost = {psi} psi {bar} bar")
+print(f"Bit Pressure Drop = {bit_pressure_drop} psi")
--- APIHydraulicsEquations/utils/conversions.py
+++ APIHydraulicsEquations/utils/conversions.py
@@ -1,10 +1,10 @@
 def feetToMeters(feet):
     return feet * 0.3048
 
+
 def inchToFeet(inch):
     return inch / 12.0
 
+
 def psiToBar(psi):
     return psi / 14.5038
-
-print(psiToBar(1422))
