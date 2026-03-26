852f4b4659c6327e4bc072b873aef986e7231de6
Billy Franscois <BillyFranscois@outlook.com>
2026-03-26 14:59:59 +0700

    Update Drilling System

--- DrillingSystem/DrillingConsole/src/Program.cs
+++ DrillingSystem/DrillingConsole/src/Program.cs
@@ -5,16 +5,16 @@ namespace DrillingConsole
 {
     class Program
     {
-        [DllImport("DrillingEngine/DrillingEngine.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("DrillingEngine.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr CreateDrillingEngine();
 
-        [DllImport("DrillingEngine/DrillingEngine.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("DrillingEngine.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern double ImperialEquivalentCirculatingDensityCalculation(IntPtr drillingEngine, double mudWeight_ppg, double pressureLoss_psi, double trueVerticalDepth_feet);
 
-        [DllImport("DrillingEngine/DrillingEngine.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("DrillingEngine.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern double MetricEquivalentCirculatingDensityCalculation(IntPtr drillingEngine, double mudWeight_ppg, double pressureLoss_psi, double trueVerticalDepth_feet);
 
-        [DllImport("DrillingEngine/DrillingEngine.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("DrillingEngine.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern void DestroyDrillingEngine(IntPtr drillingEngine);
 
         public static void ImperialEquivalentCirculatingDensityResult()
